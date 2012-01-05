20d3eaca7ba851dbc540b6c3fef32d33778d88ca
b3ni <brarcos@gmail.com>
2012-01-05 16:12:09 +0100

    comment

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ URL = 'http://www.linux-magazine.es'
 # directorio donde se guardan los artículos
 DIR_STORE = 'store'
 
-
+# usuario y pass para las suscripción digital, en blanco si no tiene
 USER = ''
 PASS = ''
